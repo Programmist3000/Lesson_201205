72ea0ba4c738c2946b67e8006d982c0ff61cd4bc
zheka <rust45111@mail.ru>
2020-12-05 12:10:56 +0500

    initial

---
+++ step_1.py
@@ -0,0 +1,15 @@
+def calc_rectangle_area(w, h) :
+    return w * h
+
+def show_rectangle_area(w, h) :
+    area = w * h
+    print('Площадь прямоугольника', area)
+    # return None
+
+...
+
+show_rectangle_area(calc_rectangle_area(15, 20))
+show_rectangle_area(calc_rectangle_area(10, 30))
+show_rectangle_area(calc_rectangle_area(5, 25))
+
+
